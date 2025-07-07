515bd69f4dd29e7595bb205b86f7abb523caa599
Ellen Davenport <62270081+eldavenport@users.noreply.github.com>
2025-07-07 11:57:08 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'Software Workshop'
-author = 'Your Name or Group'
+project = 'SIO Python for Earth Science Workshop'
+author = 'Ellen Davenport'
 release = '1.0'
 
 extensions = [
